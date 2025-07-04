671e249477cd6ed92b9a933203e74cde515a601e
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-04 02:39:35 -0600

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+def limpiar_monto(valor):
+    try:
+        return float(str(valor).replace("$", "").replace(",", ""))
+    except:
+        return 0.0
+
+def cargar_datos(ruta_csv):
+    df = pd.read_csv(ruta_csv)
+    return df
 
